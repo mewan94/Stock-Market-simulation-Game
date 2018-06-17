5520f753a42e6302c52a154f68767192eda3da08
Pubudu Ranasinghe <pubudutr@gmail.com>
2018-06-17 13:14:44 +0530

    start game function

--- sm-stockmarket/src/models/Game.js
+++ sm-stockmarket/src/models/Game.js
@@ -1,3 +1,5 @@
+import config from '../config.json'
+
 export class Game {
     playerList = [];
 
@@ -21,5 +23,19 @@ export class Game {
     
     getStock() {
 
+    }
+
+    startGame() {
+        this.startTime = Date.now();
+        this.turn = 0;
+        let timer = setInterval(() => {
+            this.turn++;
+            console.log('Starting turn ' + this.turn);
+            // calculate stocks
+            if (this.turn >= config.turns) {
+                clearInterval(timer);
+                console.log('Game ended');
+            }
+        }, config.turnTime);
     }
 }
