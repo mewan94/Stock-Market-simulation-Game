cfcaddfffd5e893b86483bbd208ce3378d8607dc
Dinuka Dilshan <ddinuka66@gmail.com>
2018-06-17 12:40:36 +0530

    Added functions to the Game.js in model

--- sm-stockmarket/src/models/Game.js
+++ sm-stockmarket/src/models/Game.js
@@ -1,10 +1,25 @@
 export class Game {
+    playerList = [];
+
     constructor(gameAdmin) {
-        this.gameAdmin = gameAdmin;   
-        this.gameID=gameAdmin;
+        this.gameAdmin = gameAdmin;
+        this.gameID = gameAdmin;
+        this.playerList.push(gameAdmin);
     }
 
-    getGameID(){
-      return this.gameID;
+    getGameID() {
+        return this.gameID;
+    }
+
+    addPlayer(player) {
+        this.playerList.push(player);
+    }
+
+    getCurrentTurn() {
+
+    }
+    
+    getStock() {
+
     }
 }
