07ec2ffdb08c518eaf09dd62722a49daa9757293
Dinuka Dilshan <ddinuka66@gmail.com>
2018-06-17 14:56:33 +0530

    Added broker functionality to broker controller

--- sm-broker/src/controllers/broker.js
+++ sm-broker/src/controllers/broker.js
@@ -4,6 +4,20 @@ export class BrokerController {
 
 
     getStock(token,gameId) {
-       
+       if(token){
+        axios.post('/games'+gameId+'/stocks', {
+            token:this.token,
+            gameId: this.gameId
+          })
+          .then(function (response) {
+           return response;
+          })
+          .catch(function (error) {
+            return new Error="error occured"
+          });
+       }
+       else{
+           return new Error="Cannot connect to the API"
+       }
     }
 }
