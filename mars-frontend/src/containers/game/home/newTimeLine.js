5bceb37090747d96eccb27d5fab8933dd51c49c7
Mewan Suriyaarachchi <kalharamewan@gmail.com>
2018-06-26 16:28:38 +0530

    changes

--- mars-frontend/src/containers/game/home/newTimeLine.js
+++ mars-frontend/src/containers/game/home/newTimeLine.js
@@ -3,20 +3,21 @@ import React from 'react';
 import {
     VerticalBarSeries,
     FlexibleWidthXYPlot,
-    FlexibleHeightXYPlot,
-    FlexibleXYPlot
+    HorizontalGridLines,
+    XAxis,
+    YAxis
 } from 'react-vis';
 
 const data = [
     {x: 0, y: 8},
     {x: 1, y: 5},
-    {x: 2, y: 4},
-    {x: 3, y: 9},
-    {x: 4, y: 1},
-    {x: 5, y: 7},
-    {x: 6, y: 6},
-    {x: 7, y: 3},
-    {x: 8, y: 2}
+    {x: 2, y: 0},
+    {x: 3, y: 0},
+    {x: 4, y: 0},
+    {x: 5, y: 0},
+    {x: 6, y: 0},
+    {x: 7, y: 0},
+    {x: 8, y: 0}
 ];
 
 const defaultProps = {
@@ -27,7 +28,10 @@ const FlexibleCharts = ({height, width}) => (
     <div>
         <div className="flexible-width" style={{width: '100%', height: '100%', border: '1px solid #ccc'}}>
             <FlexibleWidthXYPlot {...defaultProps} height={460}>
-                <VerticalBarSeries data={data} />
+                    <HorizontalGridLines />
+                    <VerticalBarSeries data={data} color="tomato"/>
+                    <XAxis title="round" />
+                    <YAxis title="value"/>
             </FlexibleWidthXYPlot>
         </div>
     </div>
