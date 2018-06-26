cf581c7043ac2b4bd7fa0ceb085210c730bd7165
Pubudu Ranasinghe <pubudutr@gmail.com>
2018-06-26 08:46:35 +0530

    buy and sell

--- sm-broker/src/api/broker.js
+++ sm-broker/src/api/broker.js
@@ -63,7 +63,16 @@ router.post('/buy', (req, res) => {
             }
         })
         .then(result => {
-            return brokerController.buyStocks(req.body.token, cost);
+            return brokerController.buyStocks(req.body.token, cost)
+            .then(r => {
+                brokerController.saveTransaction({
+                    gameid: req.body.gameid,
+                    player: player.name,
+                    type: 'buy',
+                    stock: req.body.stock,
+                    amount: req.body.amount
+                });
+            })
         })
         .then(result => {
             return brokerController.getPlayerDetails(player.name);
@@ -77,14 +86,87 @@ router.post('/buy', (req, res) => {
     })
     .catch(err => {
         res.send(responseBody);
-    })
-    
+    });
+});
 
-    // const buyStocks = brokerController.buyStocks(req.body.token, req.body.gameID);
-    // res.json(buyStocks);
-})
 router.post('/sell', (req, res) => {
-    const sellStocks = brokerController.sellStocks(req.body.token, req.body.gameID);
-    res.json(sellStocks);
-})
+    const player = jwt.decode(req.body.token);
+    let responseBody = {};
+    if (!player) {
+        res.status(403);
+        responseBody = { error: 'Not authorized' };
+    }
+    
+    if (!req.body.gameid) {
+        res.status(401);
+        responseBody = { error: 'Game id not found' };
+    }
+    
+    if (!req.body.stock) {
+        res.status(401);
+        responseBody = { error: 'Stock not found' };
+    }
+
+    if (!req.body.amount) {
+        res.status(401);
+        responseBody = { error: 'Amount not found' };
+    }
+    
+    brokerController.getStocks(req.body.token, req.body.gameid)
+    .then(stocks => {
+        let stock = stocks.find(v => v.name === req.body.stock)
+        if (!stock) {
+            res.status(401);
+            responseBody = { error: 'No such stock' };
+        };
+        return stock;
+    })
+    .then((stock => {
+        let v = brokerController.checkUserStock(player.name, stock.name, req.body.amount);
+        if (!v) {
+            res.status(400);
+            responseBody = { error: 'Buy stocks first' };
+            throw new Error('no stocks')
+        }
+        if (v.amount < req.body.amount) {
+            res.status(400);
+            responseBody = { error: 'Not enough stocks' };
+            throw new Error('Not enough stocks')
+        }
+        return stock;
+    }))
+    .then(stock => {
+        let cost = stock.price * req.body.amount;        
+        return cost;
+    })
+    .then(cost => {  
+        return brokerController.sellStocks2(req.body.token, cost) 
+        .then(r => {
+            brokerController.saveTransaction({
+                gameid: req.body.gameid,
+                player: player.name,
+                type: 'sell',
+                stock: req.body.stock,
+                amount: req.body.amount
+            });
+        })
+        .then(result => {
+            return brokerController.getPlayerDetails(player.name);
+        })
+        .then(newBalance => {
+            responseBody = newBalance;
+        })
+    })
+    .then(result => {
+        res.send(responseBody);
+    })
+    .catch(err => {
+        res.send(responseBody);
+    });
+});
+
+// router.post('/sell', (req, res) => {
+//     const sellStocks = brokerController.sellStocks(req.body.token, req.body.gameID);
+//     res.json(sellStocks);
+// })
 export default router
--- sm-broker/src/controllers/broker.js
+++ sm-broker/src/controllers/broker.js
@@ -2,32 +2,56 @@
 import config from '../config.json';
 import axios from 'axios'
 export class BrokerController {
-
+    txns = [];
 
     getStocks(token, gameId) {
         if (token) {
             console.log(config.stockmarketUrl + '/game/' + gameId + '/stocks');
-            
+
             return axios.post(config.stockmarketUrl + '/game/' + gameId + '/stocks', {
                 token: token,
                 gameId: gameId
             })
-            .then(function (response) {
-                return response.data.stocks;
-            })
-            .catch(function (error) {
+                .then(function (response) {
+                    return response.data.stocks;
+                })
+                .catch(function (error) {
 
-                return new Error(error);
-            });
+                    return new Error(error);
+                });
         }
         else {
             return new Error("Cannot connect to the API");
         }
     }
 
-    getPlayerDetails(player) {        
-        return axios.get(config.bankUrl+'/player/'+player)
-            .then(res => res.data )
+    getPlayerDetails(player) {
+        return axios.get(config.bankUrl + '/player/' + player)
+            .then(res => res.data)
+    }
+
+    saveTransaction(val) {
+        this.txns.push(val);
+    }
+
+    checkUserStock(player, stock, amount) {
+
+        let res = this.txns.filter(v => {
+            return v.stock === stock && v.type === 'buy'
+        })
+        if (res.length === 0) {
+            return false;
+        }
+        return res.reduce((acc, val) => {
+
+            return {
+                gameid: val.gameid,
+                player: val.player,
+                type: val.type,
+                stock: val.stock,
+                amount: acc.amount + val.amount
+            }
+        });
     }
 
     buyStocks(token, amount) {
@@ -37,17 +61,37 @@ export class BrokerController {
                 type: "withdraw",
                 amount: amount
             })
-            .then(function (response) {
-                return response.body;
-            })
-            .catch(function (error) {
-                return new Error("error occured while connecting to the server");
-            });
+                .then(function (response) {
+                    return response.body;
+                })
+                .catch(function (error) {
+                    return new Error("error occured while connecting to the server");
+                });
         }
         else {
             return new Error("Cannot connect to the API");
         }
     }
+
+    sellStocks2(token, amount) {
+        if (token) {
+            return axios.post(config.bankUrl + '/player/transaction', {
+                token: token,
+                type: "deposit",
+                amount: amount
+            })
+                .then(function (response) {
+                    return response.body;
+                })
+                .catch(function (error) {
+                    return new Error("error occured while connecting to the server");
+                });
+        }
+        else {
+            return new Error("Cannot connect to the API");
+        }
+    }
+
     sellStocks(token, gameId) {
         if (token) {
             axios.post(config.stockmarketUrl + '/games/' + gameId + '/stocks/sell', {
